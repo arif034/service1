e7a4b63f7f86cc99fbb2c7a4d43f9ab820bff85a
arif034 <arif.ece.jmi@gmail.com>
2022-03-02 20:48:26 +0530

    changes to service 1
    changes to service 1

---
+++ src/main/java/com/example/service1/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.service1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping("/get")
+    public String getHome()
+    {
+        return "this is homepage";
+    }
+}
